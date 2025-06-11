ec4377506d4791568e83addd34923ddf6ff5b45a
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-11 14:40:52 +0530

    feature: Allow passing command line arguments from run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ]; then
     echo "Invalid command line arguments."
-    echo "Please provide the link of .tgz file as command line argument."
+    echo "Please provide the link of .tgz file as the first command line argument."
     exit 1
 fi
 
 PACKAGE_URL="$1"
+shift
 TEMP_DIR="$(mktemp -d)"
 
 pushd "$TEMP_DIR"
@@ -14,5 +15,5 @@ curl -fsSL "$PACKAGE_URL" -o package.tgz
 npm install package.tgz
 popd
 
-npx --prefix=$TEMP_DIR ghfz
+npx --prefix="$TEMP_DIR" ghfz "$@"
 rm -rf "$TEMP_DIR"
