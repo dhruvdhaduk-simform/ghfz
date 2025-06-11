8fc81936a11bd6c63afe104f8bdf4c2737c9e58b
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-11 17:27:03 +0530

    feature: Create utility functions to handle data storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,72 @@
+import fs from 'fs';
+import path from 'path';
+import envPaths from 'env-paths';
+import { repoListSchema, userNameListSchema } from './validations';
+import { Repo } from '../types/repo';
+
+const paths = envPaths('ghfz');
+const DATA_PATH = paths.data;
+
+if (!fs.existsSync(DATA_PATH)) {
+    fs.mkdirSync(DATA_PATH);
+}
+
+const USERS_PATH = path.join(DATA_PATH, 'users.json');
+const REPOS_PATH = path.join(DATA_PATH, 'repos.json');
+
+console.log(paths.data);
+
+export function getUsers(): Array<string> {
+    let data: unknown;
+
+    try {
+        if (fs.existsSync(USERS_PATH)) {
+            data = JSON.parse(fs.readFileSync(USERS_PATH).toString());
+        }
+    } catch (err) {}
+
+    const parsed = userNameListSchema.safeParse(data);
+
+    if (parsed.success) {
+        return parsed.data;
+    }
+
+    return [];
+}
+
+export function getRepos(): Array<Repo> {
+    let data: unknown;
+
+    try {
+        if (fs.existsSync(REPOS_PATH)) {
+            data = JSON.parse(fs.readFileSync(REPOS_PATH).toString());
+        }
+    } catch (err) {}
+
+    const parsed = repoListSchema.safeParse(data);
+
+    if (parsed.success) {
+        return parsed.data;
+    }
+
+    return [];
+}
+
+export function addRepos(username: string, repoNames: Array<string>) {
+    const users = getUsers();
+
+    if (!users.includes(username)) {
+        users.push(username);
+    }
+
+    let repos = getRepos();
+
+    repos = repos.filter((repo) => repo.username !== username);
+
+    repoNames.forEach((reponame) => {
+        repos.push({ username, reponame });
+    });
+
+    fs.writeFileSync(USERS_PATH, JSON.stringify(users));
+    fs.writeFileSync(REPOS_PATH, JSON.stringify(repos));
+}
